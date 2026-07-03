bf047ba16dc52204f82cbd88b269a999c40790a2
Kobe <irissneo@gmail.com>
2026-07-03 18:32:54 +0200

    log origins to see what cors is blocking and allowing

--- backend/middleware/cors.js
+++ backend/middleware/cors.js
@@ -6,7 +6,7 @@ const allowedOrigins = [
   'http://localhost:5500',
   'http://127.0.0.1:5500',
   'http://localhost:3000',
-  process.env.FRONTEND_URL,
+  'https://justbook-it.netlify.app',
 ].filter(Boolean);
 
 const corsMiddleware = cors({
