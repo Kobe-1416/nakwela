969a817447636b4d9d78edf68e3d5087d8938888
Kobe <irissneo@gmail.com>
2026-07-05 17:49:01 +0200

    change backend name to nakwela and make relevant changes in code base

--- frontend/config.js
+++ frontend/config.js
@@ -4,4 +4,4 @@ const API_URL =
   location.hostname === "localhost" ||
   location.hostname === "127.0.0.1"
     ? "http://localhost:3000/api"
-    : "https://township-booking.onrender.com/api";
+    : "https://nakwela.onrender.com/api";
